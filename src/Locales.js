7464addad70cb91191bedc36f55e525961756ba8
FrancoPetosa99 <franco.petosa15@gmail.com>
2021-05-22 19:14:38 -0300

    ECO BOLSA 22/05

--- src/Locales.js
+++ src/Locales.js
@@ -22,7 +22,7 @@ function Locales(props) {
                     Some quick example text to build on the card title and make up the bulk of
                     the card's content.
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Button variant="successs">Go somewhere</Button>
                 </Card.Body>
             </Card>
             </Col>
