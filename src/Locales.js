22a63ce4edbf5a1104bbe91254bfda0eb2bfedde
Ezequiel Agustin Ibarra <ibarra.ezequiel01@gmail.com>
2021-05-22 18:24:17 -0300

    BOLSA ECOLOGICA MAP DE LOCALES

--- src/Locales.js
+++ src/Locales.js
@@ -2,15 +2,32 @@ import React,{useState,useEffect} from "react"
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import data from './data/data'
 
 function Locales(props) {
+    const {locales} = data;
     return(
         <Container fluid>
         <Row >
-            <Col className="locales">BOEDO</Col>
-            <Col className="locales">ALMAGRO</Col>
-            <Col className="locales">BARRACAS</Col>
-            <Col className="locales">SAN TELMO</Col>
+            
+            {locales.map((local)=>(
+            <Col >
+                <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="assets/almagro.png" />
+                <Card.Body>
+                    <Card.Title>{local.name}</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            </Col>
+            ))}
+            
         </Row>
       </Container>
     )
