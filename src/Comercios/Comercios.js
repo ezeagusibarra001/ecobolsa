c889076f5e821455a4c2de107673b19135a8ec02
FrancoPetosa99 <franco.petosa15@gmail.com>
2021-06-06 17:48:04 -0300

    ECO BOLSA 6/6

--- src/Comercios/Comercios.js
+++ src/Comercios/Comercios.js
@@ -3,6 +3,7 @@ import Layout from "../Layout";
 import Carrusel from "../Carrusel"
 import Banner from "./Banner"
 import Locales from "./Locales"
+import "./Comercios.css"
 
 
 function Comercio(props) {
@@ -17,36 +18,64 @@ function Comercio(props) {
                 id: 0,
                 name: "Boedo",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"https://www.instagram.com/francopetosa__/?hl=es-la",
+                wp:"",
             },
             {
                 id: 1,
                 name: "Almagro",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"",
+                wp:""
             },
             {
                 id: 2,
                 name: "Monserrat",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"",
+                wp:""
             },
             {
                 id: 3,
                 name: "Deboto",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"",
+                wp:""
             },
             {
                 id: 4,
                 name: "V.Parque",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"",
+                wp:""
             },
             {
                 id: 5,
                 name: "Caballito",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"",
+                wp:""
             },
             {
                 id: 6,
                 name: "Palermo",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"",
+                wp:""
+            },
+            {
+                id: 7,
+                name: "Quilmes",
+                description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"",
+                wp:""
+            },
+            {
+                id: 8,
+                name: "Libertilandia",
+                description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                instagram:"",
+                wp:""
             },
         ]
   
@@ -57,7 +86,9 @@ function Comercio(props) {
             <Layout>
                 <Carrusel img1={img1} img2={img2} img4={img4}/>
                 <Banner/>
-                {locales.map(locales=><Locales data={locales}  />)}
+                <div className="ContainerLocales">
+                    {locales.map(locales=><Locales data={locales}  />)}
+                </div>
             </Layout>
         </div>
     )
