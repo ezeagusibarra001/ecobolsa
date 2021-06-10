445c887acad71113b3f473b0aa3f7f42969cb783
Ezequiel Agustin Ibarra <ibarra.ezequiel01@gmail.com>
2021-06-10 19:53:25 -0300

    Update Video.js

--- src/Home/Video.js
+++ src/Home/Video.js
@@ -8,7 +8,7 @@ function Video(props) {
     return (
 
         <div className="Video" id="Video">
-            <iframe className="reallyVideo"  src="https://www.youtube.com/embed/Ycs7gq_fRcA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className="reallyVideo" src="https://www.youtube.com/embed/vs0dJ0XwwJI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 
